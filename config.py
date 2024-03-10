afa5469b5f596d5716fcbafa9653715dfde065f8
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-03-10 18:30:48 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = "6991045098:AAHQWo0ifQ2ItERlhOca5Ubi5_nM3BvlNII"
+    API_ID = "26945844"
+    API_HASH = "9f2f1d81fd2af2b21a700fa6681215b1"
+    AUTH_USERS = '5934830127,1085174050'
+
