237d44ae6b5f1ca08d562de5e8c83b3ffe845849
Kratik00 <159747619+Kratik00@users.noreply.github.com>
2025-05-06 06:31:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6991045098:AAHQWo0ifQ2ItERlhOca5Ubi5_nM3BvlNII"
-    API_ID = "26945844"
-    API_HASH = "9f2f1d81fd2af2b21a700fa6681215b1"
-    AUTH_USERS = '5934830127,1085174050'
+    BOT_TOKEN = ""
+    API_ID = "25318125"
+    API_HASH = "b29fb6a928e8b8a3308f8c2d3ba9cfb0"
+    AUTH_USERS = '7764674199'
 
